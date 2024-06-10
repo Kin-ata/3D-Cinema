bf83aa8a380808d3a25e6175789cf0e8c17cc8ec
Kinata <86466191+Kin-ata@users.noreply.github.com>
2024-06-11 02:46:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
+import { OBJLoader } from 'https://threejs.org/examples/jsm/loaders/OBJLoader.js';
+import { CSS3DRenderer, CSS3DObject } from 'https://orvillechomer.github.io/miscJsFiles/THREEJS/r120/jsm/renderers/CSS3DRenderer.js';
+
+let model;
 
 function init() {
     var scene = new THREE.Scene();
     scene.background = new THREE.Color(0x000000);
     // var gui = new dat.GUI();
+
+    var loader = new OBJLoader();
+    loader.load('chair2.obj', function (object) {
+        object.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+                child.material = new THREE.MeshStandardMaterial({ color: 'black' });
+            }
+        });
+        // save object to global variable
+        model = object;
+    });
+    // setTimeout(function() {
+    //     var chairTest = Chair2(model,{ position: [0, 1, 2], scale: 0.2 });
+    //     chairTest.name = 'chairTest';
+    //     scene.add(chairTest);
+    
+    //     var chairTest2 = Chair2(model,{ position: [0, 1, 4], scale: 0.2 });
+    //     chairTest2.name = 'chairTest2';
+    //     scene.add(chairTest2);
+    // }, 15000);
 
     var room = Room();
     room.name = 'room';
@@ -14,31 +38,57 @@ function init() {
     screen.name = 'screen';
     scene.add(screen);
 
+    var ambientLight = new THREE.AmbientLight('rgb(255, 255, 255)', 0.5);
+    scene.add(ambientLight);
+
+
     // const alphabetic = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     const alphabetic = ['K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A']
     var chairs = [[...Array(25)], [...Array(25)], [...Array(25)], [...Array(25)], [...Array(25)], [...Array(25)], [...Array(25)], [...Array(25)], [...Array(25)], [...Array(25)], [...Array(25)]].map( (row, index) => {
-            
+        var temp = 0;
             return row.map( (seat, i) => {
                 let seatPostion = 13 - i;
-                return {
-                    id: `${index} - ${i}`, 
-                    position: new THREE.Vector3(-1 * (-1 + seatPostion) * 0.2, (0.5 * (-index + 11) ) * 0.2, index * 1.5  * 0.2), 
-                    target:   new THREE.Vector3(-1 * (-1 + seatPostion)  * 0.2, (0.6 )  * 0.2, (-0.355) * 0.2),
-                    seat: `${alphabetic[index]}${i+1}`
+                if (i === 5 || i === 6 || i === 18 || i === 19) {
+                    temp ++;
+                    return {
+                        id: 'stairBlock',
+                        position: new THREE.Vector3(-1 * (-1 + seatPostion) * 0.2, (0.5 * (-index + 11) ) * 0.2, index * 1.5  * 0.3)
+                    }
                 }
-            })
+                else {
+                    return {
+                        id: `${index} - ${i - temp}`, 
+                        position: new THREE.Vector3(-1 * (-1 + seatPostion) * 0.2, (0.5 * (-index + 11) ) * 0.2, index * 1.5  * 0.3), 
+                        target:   new THREE.Vector3(-1 * (-1 + seatPostion)  * 0.2, (0.6 )  * 0.2, (-0.355) * 0.3),
+                        seat: `${alphabetic[index]}${i+1-temp}`
+                    }
+                }
+            }
+        )
     
-        }).flat(Infinity)
+    }).flat(Infinity)
+    
 
-    chairs.forEach(chair => {
-        var chairMesh = Chair({ position: chair.position.toArray(), scale: 0.2 });
-        chairMesh.name = chair.id;
-        scene.add(chairMesh);
-    });
+    setTimeout(function() {
+        console.log('model is loaded');
+        chairs.forEach(chair => {
+            if (chair.id === 'stairBlock') {
+                var stairBlock = StairBlock({ position: chair.position.toArray() });
+                stairBlock.name = chair.id;
+                scene.add(stairBlock);
+            }
+            else {
+                // var chairMesh = Chair({ position: chair.position.toArray(), scale: 0.2 });
+                var chairMesh = Chair2(model,{ position: chair.position.toArray(), scale: 0.3 });
+                chairMesh.name = chair.id;
+                scene.add(chairMesh);
+            }
+        });
+    }, 1500);
 
     var settings = {
-        row: 0,
-        col: 13
+        row: -1,
+        col: 11
     };
     var target = `${alphabetic[settings.row]}${settings.col}`;
     
@@ -53,6 +103,7 @@ function init() {
     camera.lookAt(new THREE.Vector3(0, 1, 7));
 
     var renderer = new THREE.WebGLRenderer();
+    renderer.shadowMap.enabled = true;
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor('rgb(255, 255, 255)');
     document.getElementById('webgl').appendChild(renderer.domElement);
@@ -60,43 +111,6 @@ function init() {
     var controls = new OrbitControls(camera, renderer.domElement);
 
     controls.target.set(0, 1.998, -0.99);
-
-    // // dat.GUI
-    // var cameraFolder = gui.addFolder('Camera');
-    // cameraFolder.add(camera.position, 'x', -10, 10);
-    // cameraFolder.add(camera.position, 'y', -10, 10);
-    // cameraFolder.add(camera.position, 'z', -10, 10);
-    // // cameraFolder.open();
-    // var targetFolder = gui.addFolder('Target');
-    // targetFolder.add(settings, 'row', 0, 10).step(1).onChange(function(val) {
-    //     settings.row = val;
-    //     target = `${alphabetic[val]}${settings.col}`;
-    //     chairs.forEach(chair => {
-    //         if (chair.seat == target) {
-    //             camera.position.set(chair.position.x, chair.position.y + 0.2, chair.position.z);
-    //             // controls.target.set(0, 1, 7);
-    //             // controls.target.set(chair.target.x, chair.target.y, chair.target.z);
-    //             controls.target.set(chair.position.x + (0-chair.position.x) * 0.01, chair.position.y + 0.2 + (1-chair.position.y) * 0.01, chair.position.z+(7-chair.position.z) * 0.01);
-    //         }
-    //     });
-
-    // });
-    // targetFolder.add(settings, 'col', 0, 24).step(1).onChange(function(val) {
-    //     settings.col = val;
-    //     target = `${alphabetic[settings.row]}${val}`;
-    //     chairs.forEach(chair => {
-    //         if (chair.seat == target) {
-    //             camera.position.set(chair.position.x, chair.position.y + 0.2, chair.position.z);
-    //             // controls.target.set(0, 1, 7);
-    //             // controls.target.set(chair.target.x, chair.target.y, chair.target.z);
-    //             controls.target.set(chair.position.x + (0-chair.position.x) * 0.01, chair.position.y + 0.2 + (1-chair.position.y) * 0.01, chair.position.z+(7-chair.position.z) * 0.01);
-    //         }
-    //     }
-    //     );
-
-    // });
-    // targetFolder.open();
-    // // add 1 value for isCameraMovementEnabled is boolean value
 
     window.addEventListener('resize', function() {
         var width = window.innerWidth;
@@ -117,8 +131,8 @@ function init() {
             target = `${alphabetic[settings.row]}${settings.col}`;
             chairs.forEach(chair => {
                 if (chair.seat == target) {
-                    camera.position.set(chair.position.x, chair.position.y + 0.2, chair.position.z);
-                    controls.target.set(chair.position.x + (0-chair.position.x) * 0.01, chair.position.y + 0.2 + (1-chair.position.y) * 0.01, chair.position.z+(7-chair.position.z) * 0.01);
+                    camera.position.set(chair.position.x, chair.position.y + 0.35, chair.position.z);
+                    controls.target.set(chair.position.x + (0-chair.position.x) * 0.01, chair.position.y + 0.35 + (1-chair.position.y) * 0.01, chair.position.z+(7-chair.position.z) * 0.01);
                 }
             });
         } else if (keyCode == 39) {
@@ -130,8 +144,8 @@ function init() {
             target = `${alphabetic[settings.row]}${settings.col}`;
             chairs.forEach(chair => {
                 if (chair.seat == target) {
-                    camera.position.set(chair.position.x, chair.position.y + 0.2, chair.position.z);
-                    controls.target.set(chair.position.x + (0-chair.position.x) * 0.01, chair.position.y + 0.2 + (1-chair.position.y) * 0.01, chair.position.z+(7-chair.position.z) * 0.01);
+                    camera.position.set(chair.position.x, chair.position.y + 0.35, chair.position.z);
+                    controls.target.set(chair.position.x + (0-chair.position.x) * 0.01, chair.position.y + 0.35 + (1-chair.position.y) * 0.01, chair.position.z+(7-chair.position.z) * 0.01);
                 }
             });
         } else if (keyCode == 38) {
@@ -143,8 +157,8 @@ function init() {
             target = `${alphabetic[settings.row]}${settings.col}`;
             chairs.forEach(chair => {
                 if (chair.seat == target) {
-                    camera.position.set(chair.position.x, chair.position.y + 0.2, chair.position.z);
-                    controls.target.set(chair.position.x + (0-chair.position.x) * 0.01, chair.position.y + 0.2 + (1-chair.position.y) * 0.01, chair.position.z+(7-chair.position.z) * 0.01);
+                    camera.position.set(chair.position.x, chair.position.y + 0.35, chair.position.z);
+                    controls.target.set(chair.position.x + (0-chair.position.x) * 0.01, chair.position.y + 0.35 + (1-chair.position.y) * 0.01, chair.position.z+(7-chair.position.z) * 0.01);
                 }
             });
         } else if (keyCode == 40) {
@@ -156,8 +170,8 @@ function init() {
             target = `${alphabetic[settings.row]}${settings.col}`;
             chairs.forEach(chair => {
                 if (chair.seat == target) {
-                    camera.position.set(chair.position.x, chair.position.y + 0.2, chair.position.z);
-                    controls.target.set(chair.position.x + (0-chair.position.x) * 0.01, chair.position.y + 0.2 + (1-chair.position.y) * 0.01, chair.position.z+(7-chair.position.z) * 0.01);
+                    camera.position.set(chair.position.x, chair.position.y + 0.35, chair.position.z);
+                    controls.target.set(chair.position.x + (0-chair.position.x) * 0.01, chair.position.y + 0.35 + (1-chair.position.y) * 0.01, chair.position.z+(7-chair.position.z) * 0.01);
                 }
             });
         }
@@ -170,54 +184,16 @@ function init() {
     return scene;
 }
 
-function Wall({ position, rotation, color, transparent = false, opacity = 1, args }) {
+function Wall({ position, rotation, color, isTexture =false, transparent = false, opacity = 1, args }) {
     var wall = new THREE.Group();
 
-    var plane = getPlane(args[0], args[1], color);
+    var plane = getPlane(args[0], args[1], color, isTexture);
     plane.position.set(position[0], position[1], position[2]);
     plane.rotation.set(rotation[0], rotation[1], rotation[2]);
     plane.name = 'wall';
     wall.add(plane);
 
     return wall;
-}
-
-function Stage({ position }) {
-    var stage = new THREE.Group();
-
-    var mainStage = getBox(5, 0.5, 2, 'gray'); // Assuming mainStage remains gray
-    mainStage.position.set(position[0], position[1], position[2]);
-    mainStage.name = 'mainStage';
-    stage.add(mainStage);
-
-    var stairColor = 'brown'; // Example color for stairs
-
-    var stair1 = getBox(0.4, 0.1, 0.2, stairColor);
-    stair1.position.set(position[0] - 1.4, position[1] - 0.1, position[2] + 1);
-    stair1.name = 'stair1';
-    stage.add(stair1);
-
-    var stair2 = getBox(0.4, 0.1, 0.2, stairColor);
-    stair2.position.set(position[0] - 1.4, position[1], position[2] + 1.2);
-    stair2.name = 'stair2';
-    stage.add(stair2);
-
-    var stair3 = getBox(0.4, 0.1, 0.2, stairColor);
-    stair3.position.set(position[0] - 1.4, position[1] - 0.1, position[2] + 1.3);
-    stair3.name = 'stair3';
-    stage.add(stair3);
-
-    var stair4 = getBox(0.4, 0.1, 0.2, stairColor);
-    stair4.position.set(position[0] - 1.4, position[1] - 0.2, position[2] + 1.3);
-    stair4.name = 'stair4';
-    stage.add(stair4);
-
-    var stair5 = getBox(0.4, 0.1, 0.2, stairColor);
-    stair5.position.set(position[0] - 1.4, position[1] - 0.3, position[2] + 1.4);
-    stair5.name = 'stair5';
-    stage.add(stair5);
-
-    return stage;
 }
 
 function CinemaScreen({ position, videoUrl, muted }) {
@@ -305,52 +281,134 @@ function Chair({ position, onClick, scale, isSeatSelected }) {
     ground.position.set(position[0], position[1] - 0.6, position[2]); // Adjusted Y position
     ground.name = 'ground';
     ground.receiveShadow = true;
-
     chair.add(ground);
 
 
     return chair;
 }
 
+function Chair2(model,{ position, onClick, scale, isSeatSelected }) {
+    // console.log(model);
+    // const rotationY = Math.PI; // Rotate 180 degrees to face the screen
+    if (model === undefined) {
+        var loader = new OBJLoader();
+        var chair = new THREE.Group();
+        loader.load('chair2.obj', function (object) {
+            object.traverse(function (child) {
+                if (child instanceof THREE.Mesh) {
+                    child.material = new THREE.MeshStandardMaterial({ color: 'black' });
+                }
+            });
+        // save object to global variable
+        model = object;
+        chair.add(model);
+        chair.position.set(position[0], position[1], position[2]);
+        chair.scale.set(scale, scale, scale);
+        // chair.rotation.set(0, rotationY, 0);
+        chair.onClick = onClick;
+        chair.isSeatSelected = isSeatSelected;
+        });
+    }
+    else {
+        var chair = model.clone();
+        chair.position.set(position[0], position[1], position[2]);
+        chair.scale.set(scale, scale, scale);
+        // chair.rotation.set(0, rotationY, 0);
+        chair.onClick = onClick;
+        chair.isSeatSelected = isSeatSelected;
+
+        // Load the texture
+        var loader = new THREE.TextureLoader();
+        loader.load('chair.jpg', function(texture) {
+            texture.wrapS = THREE.RepeatWrapping;
+            texture.wrapT = THREE.RepeatWrapping;
+            texture.repeat.set(4, 4);
+
+            // Apply the texture to each mesh in the chair model
+            chair.traverse(function (child) {
+                if (child instanceof THREE.Mesh) {
+                    child.castShadow = true;
+                    // Apply the texture to the material of the mesh
+                    child.material = new THREE.MeshStandardMaterial({ map: texture , color: 0x666666});
+                }
+            });
+        });
+    }
+
+    return chair;
+}
+
+function StairBlock({ position}) {
+    var stairBlock = getBox(0.2, 0.05, 0.225, 'grey');
+    stairBlock.position.set(position[0], position[1] - 0.075, position[2] + 0.43);
+    stairBlock.name = 'stairBlock';
+    stairBlock.receiveShadow = true;
+    return stairBlock;
+}
+
 function Room() {
     var room = new THREE.Group();
 
-    var floor = Wall({ position: [0, 0, 2.5], rotation: [-Math.PI / 2, 0, 0], color: 'grey', args: [5, 8] });
+    var floor = Wall({ position: [0, 0, 2.5], rotation: [-Math.PI / 2, 0, 0], color: 'grey', isTexture: true, args: [5, 8] });
     floor.name = 'floor';
     room.add(floor);
 
-    var ceiling = Wall({ position: [0, 3, 2.5], rotation: [Math.PI / 2, 0, 0], color: 'grey', args: [5, 8] });
+    var ceiling = Wall({ position: [0, 3, 2.5], rotation: [Math.PI / 2, 0, 0], color: 'grey', isTexture: true, args: [5, 8] });
     ceiling.name = 'ceiling';
+    ceiling.receiveShadow = true;
     room.add(ceiling);
 
-    var lightFrontLeft = getSpotLight(10, 'rgb(255, 255, 255)');
-    lightFrontLeft.position.set(-2, 3, 2);
-    lightFrontLeft.name = 'lightFrontLeft';
-    room.add(lightFrontLeft);
+    var stage = getBox(5, 0.1, 1.69, 'dimgrey');
+    stage.position.set(0, 0.05, 5.6705);
+    stage.name = 'stage';
+    stage.receiveShadow = true;
+    room.add(stage);
 
-    var lightFrontRight = getSpotLight(10, 'rgb(255, 255, 255)');
-    lightFrontRight.position.set(2, 3, 2);
-    lightFrontRight.name = 'lightFrontRight';
-    room.add(lightFrontRight);
+    // var lightFrontLeft = getPointLight(2, 'rgb(255, 255, 255)');
+    // lightFrontLeft.position.set(-1, 2, 2);
+    // lightFrontLeft.name = 'lightFrontLeft';
+    // room.add(lightFrontLeft);
 
-    var lightBackLeft = getPointLight(1, 'rgb(255, 255, 255)');
-    lightBackLeft.position.set(-2, 3, 6);
-    lightBackLeft.name = 'lightBackLeft';
-    room.add(lightBackLeft);
+    // var lightFrontRight = getPointLight(2, 'rgb(255, 255, 255)');
+    // lightFrontRight.position.set(1, 2, 2);
+    // lightFrontRight.name = 'lightFrontRight';
+    // room.add(lightFrontRight);
 
-    var lightBackRight = getPointLight(1, 'rgb(255, 255, 255)');
-    lightBackRight.position.set(2, 3, 6);
-    lightBackRight.name = 'lightBackRight';
-    room.add(lightBackRight);
+    // var lightBackLeft = getPointLight(1, 'rgb(255, 255, 255)');
+    // lightBackLeft.position.set(-2, 3, 6);
+    // lightBackLeft.name = 'lightBackLeft';
+    // room.add(lightBackLeft);
+
+    // var lightBackRight = getPointLight(1, 'rgb(255, 255, 255)');
+    // lightBackRight.position.set(2, 3, 6);
+    // lightBackRight.name = 'lightBackRight';
+    // room.add(lightBackRight);
+
+    var centerLight = getPointLight(10, 'rgb(255, 255, 255)');
+    centerLight.position.set(0, 2, 5);
+    centerLight.name = 'centerLight';
+    room.add(centerLight);
+
 
     var backWall = Wall({ position: [0, 1.5, -1.5], rotation: [Math.PI, Math.PI, 0], color: 'dimgrey', args: [5, 3] });
     backWall.name = 'backWall';
     room.add(backWall);
 
-    var backStage = getBox(5, 1.1, 1.4, 'grey');
-    backStage.position.set(0, 0.55, -0.8);
+    var backStage = getBox(5, 1.1, 1.45, 'grey');
+    backStage.position.set(0, 0.55, -0.830);
     backStage.name = 'backStage';
+    backStage.receiveShadow = true;
     room.add(backStage);
+
+
+
+    for (let i = 1; i <= 11; i++) {
+        var subStage = getBox(5, 1.2 - (i * 0.1), i * 0.45, 'grey');
+        subStage.position.set(0, 0.6 - (i * 0.05), i*0.225 - 0.125);
+        // subStage.name = `subStage${i}`;
+        subStage.receiveShadow = true;
+        room.add(subStage);
+    }
 
     var frontWall = Wall({ position: [0, 1.5, 6.5], rotation: [Math.PI, 0, 0], color: 'dimgrey', args: [5, 3] });
     frontWall.name = 'frontWall';
@@ -371,17 +429,28 @@ function Room() {
 function getBox(w, h, d, color = 0x8b0000) {
     var geometry = new THREE.BoxGeometry(w, h, d);
     var material = getMaterial('standard', color);
+    
     var mesh = new THREE.Mesh(
         geometry,
         material
     );
-    mesh.castShadow = true;
-    mesh.receiveShadow = true;
+    mesh.castShadow = false;
+    mesh.receiveShadow = false;
 
+    var loader = new THREE.TextureLoader();
+    loader.load('wall.jpg', function(texture) {
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set(4, 4);
+        mesh.material.map = texture;
+        mesh.material.needsUpdate = true;
+    }
+    );
+    
     return mesh;
 }
 
-function getPlane(w, h, color = 0xffffff) {
+function getPlane(w, h, color = 0xffffff, isTexture = false) {
     var geometry = new THREE.PlaneGeometry(w, h);
     var material = getMaterial('standard', color);
     var mesh = new THREE.Mesh(
@@ -390,6 +459,18 @@ function getPlane(w, h, color = 0xffffff) {
     );
     mesh.receiveShadow = true;
     mesh.castShadow = true;
+
+    if (isTexture) {
+        var loader = new THREE.TextureLoader();
+        loader.load('wall.jpg', function(texture) {
+            texture.wrapS = THREE.RepeatWrapping;
+            texture.wrapT = THREE.RepeatWrapping;
+            texture.repeat.set(4, 4);
+            mesh.material.map = texture;
+            mesh.material.needsUpdate = true;
+        }
+        );
+    }
 
     return mesh;
 }
